ab34ef6be37af10c2057693b0cb802460bb14214
marcy <marc326marcy@gmail.com>
2024-12-30 02:27:56 +0900

    [feature] ワークフローに反映

--- infra/modules/rds/outputs.tf
+++ infra/modules/rds/outputs.tf
@@ -2,9 +2,9 @@ output "cluster_arn" {
   value = aws_rds_cluster.cluster.arn
 }
 
-# output "secret_arn" {
-#   value = aws_rds_cluster.cluster.master_user_secret[0].secret_arn
-# }
+output "secret_arn" {
+  value = aws_rds_cluster.cluster.master_user_secret[0].secret_arn
+}
 
 output "database_name" {
   value = var.aurora_database_name
